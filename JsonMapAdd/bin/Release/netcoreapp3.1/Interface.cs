576db92f2f84b97f6aea71aa4cb375a97b0a191c
artur.trombim <artur.trombim@itravel.com.br>
2024-07-16 12:50:11 -0300

    [ADD] Relatorio de aereos diarios | [DOC] Melhoria na documentacao

---
+++ JsonMapAdd/bin/Release/netcoreapp3.1/Interface.cs
@@ -0,0 +1,175 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JsonMapAdd
+{
+    public partial class Interface : Form
+    {
+        #region INTERFACE
+        public Interface()
+        {
+            InitializeComponent();
+            
+            foreach(MappingData.typeAction tipo in Enum.GetValues(typeof(MappingData.typeAction)))
+            {
+                tipoBuscaTxt.Items.Add(tipo);
+            }
+            attJsonList();
+        }
+
+        private void filtroLabel_Click(object sender, EventArgs e)
+        {
+            enableAddButton();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+
+            List<string> filtro = new List<string> { filterOneTxt.Text, filterTwoTxt.Text };
+            List<MappingData> dadosList = MappingJsonRead();
+            MappingData newMap = new MappingData
+            {
+                Tipo = tipoBuscaTxt.SelectedIndex,
+                Filtro = filtro,
+                Chave = keyText.Text,
+                Mudar = changeText.Text,
+            };
+            dadosList.Add(newMap);
+            OverrideJson(dadosList);
+            attJsonList();
+        }
+
+        private void attBtn_Click(object sender, EventArgs e)
+        {
+            attJsonList();
+        }
+
+        private void tipoBuscaTxt_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Verifica se o tipo é direct, pois no direct não tem Chave
+            if (tipoBuscaTxt.SelectedIndex == 3) {
+                keyText.Text = "NOTHING";
+                keyText.Enabled = false;
+            }
+            else { keyText.Enabled = true;}
+            enableAddButton();
+        }
+
+        private void changeText_TextChanged(object sender, EventArgs e)
+        {
+            enableAddButton();
+        }
+
+        private void keyText_TextChanged(object sender, EventArgs e)
+        {
+            enableAddButton();
+        }
+
+        private void filterOneTxt_TextChanged(object sender, EventArgs e)
+        {
+            enableAddButton();
+            if (filterOneTxt.Text.Length <= 0) { filterTwoTxt.Enabled = false; filterTwoTxt.Text = null; }
+            else { filterTwoTxt.Enabled = true; }
+        }
+
+        private void slctRmvBtn_Click(object sender, EventArgs e)
+        {
+            List<MappingData> dadosList = MappingJsonRead();
+            int aux = 0;
+            foreach (int index in JsonList.CheckedIndices)
+            {
+                dadosList.RemoveAt(index);
+                aux+= 1;
+            };
+            OverrideJson(dadosList);
+            attJsonList();
+        }
+        #endregion
+        
+        #region METODOS AUXILIARES
+
+        /// <summary>
+        /// Metodo para garantir que o botão de adicionar só seja habilitado na hora certa.
+        /// </summary>
+        private void enableAddButton()
+        {
+            if (changeText.Text.Length > 0 && keyText.Text.Length > 0 && filterOneTxt.Text.Length > 0 && tipoBuscaTxt.SelectedIndex != -1)
+            {
+                AddBtn.Enabled = true;
+            }
+            else AddBtn.Enabled = false;
+        }
+        
+        /// <summary>
+        /// Metodo que retnorna o diretorio até o ponto definido. (target)
+        /// </summary>
+        /// <param name="currentDirectory"></param>
+        /// <param name="targetDirectory"></param>
+        /// <returns></returns>
+        public static string FindTargetDirectory(string currentDirectory, string targetDirectory)
+        {
+            string currentDirName = new DirectoryInfo(currentDirectory).Name;
+
+            if (string.Equals(currentDirName, targetDirectory, StringComparison.OrdinalIgnoreCase))
+            {
+                return currentDirectory;
+            }
+
+            DirectoryInfo parentDirectory = Directory.GetParent(currentDirectory);
+
+            if (parentDirectory == null)
+            {
+                return null;
+            }
+
+            return FindTargetDirectory(parentDirectory.FullName, targetDirectory);
+        }
+
+        /// <summary>
+        /// Metodo que trata o JSON para que possa ser manipulado de forma facil.
+        /// Aqui passamos o diretorio de forma dinamica.
+        /// </summary>
+        /// <returns></returns>
+        public static List<MappingData> MappingJsonRead()
+        {
+            string path = FindTargetDirectory(AppDomain.CurrentDomain.BaseDirectory, "GeradorDeCodigo");
+            path = path + @"\InterfaceWebConfig\Json\Mapping.json";
+            string jsonMap = File.ReadAllText(path);
+            List<MappingData> dadosList = JsonConvert.DeserializeObject<List<MappingData>>(jsonMap);
+
+            return dadosList;
+        }
+
+        /// <summary>
+        /// Metodo que garante a atualização de forma correta do JSON, deve ser sempre utilizado
+        /// para garantir que o programa irá funcionar corretamente.
+        /// </summary>
+        /// <param name="json"></param>
+        public static void OverrideJson(List<MappingData> json)
+        {
+            File.WriteAllText(FindTargetDirectory(AppDomain.CurrentDomain.BaseDirectory, "GeradorDeCodigo") + @"\InterfaceWebConfig\Json\Mapping.json", System.Text.Json.JsonSerializer.Serialize(json));
+        }
+        /// <summary>
+        /// Metodo responsavel por atualizar a lista de json na tela do app
+        /// </summary>
+        public void attJsonList()
+        {
+            JsonList.Items.Clear();
+            foreach (var dado in MappingJsonRead())
+            {
+                JsonList.Items.Add($"Filtro:  {dado.Filtro[0]}, {dado.Filtro[1]}");
+            }
+        }
+        #endregion
+    }
+}
